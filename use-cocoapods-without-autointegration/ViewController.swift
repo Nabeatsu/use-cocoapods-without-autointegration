e9e48821d3befe87b3b0e015db49728886f9eea1
nabeatsu <tanabe.1478@gmail.com>
2019-12-03 21:27:19 +0900

    Initial commit

---
+++ use-cocoapods-without-autointegration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  use-cocoapods-without-autointegration
+//
+//  Created by tanabe.nobuyuki on 2019/12/03.
+//  Copyright © 2019 tanabe.nobuyuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
